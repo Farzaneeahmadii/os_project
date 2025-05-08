0293e8a87f241f9d8b613eb1348d6f44f1f69af1
erfaninajafi <erfan.najafi8213@gmail.com>
2025-05-09 01:10:49 +0330

    fix logger

--- kernel/custom_logger.h
+++ kernel/custom_logger.h
@@ -1,6 +1,10 @@
 #ifndef _CUSTOM_LOGGER_H_
 #define _CUSTOM_LOGGER_H_
 
+#define LOG_LEVEL_INFO 1
+#define LOG_LEVEL_WARN 2
+#define LOG_LEVEL_ERROR 3
+
 void log_message(int level, const char *message);
 
-#endif // _CUSTOM_LOGGER_H_
+#endif
